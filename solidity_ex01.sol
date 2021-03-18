9c3c8228090341443b37d5ff2b94789d2b084b36
tghjkl777 <50409023+tghjkl777@users.noreply.github.com>
2021-03-18 14:21:53 +0900

    Add files via upload

--- solidity_ex01.sol
+++ solidity_ex01.sol
@@ -1,8 +1,12 @@
 pragma solidity ^0.4.11;
 
 contract Example005 {
-  function operation() returns (uint result) {
-    result = sum(11,5);
+  function operation() returns (uint sum_res, uint sub_res , uint mul_res,uint dev_res) {
+    sum_res = sum(11,5);
+    sub_res = sum(11,5);
+    mul_res = sum(11,5);
+    dev_res = sum(11,5);
+      
   }
 
   function sum(uint a, uint b) returns (uint) {
