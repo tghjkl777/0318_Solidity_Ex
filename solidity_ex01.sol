f91d0190916a0d2b264fddbfa8095ae7fff17017
tghjkl777 <50409023+tghjkl777@users.noreply.github.com>
2021-03-18 14:15:03 +0900

    Add files via upload

---
+++ solidity_ex01.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.11;
+
+contract Example005 {
+  function operation() returns (uint result) {
+    result = sum(11,5);
+  }
+
+  function sum(uint a, uint b) returns (uint) {
+      uint result = a + b;
+      return result;
+    }
+  function sub(uint a, uint b) returns(uint){
+      uint result =a-b;
+      return result;
+  }
+  
+    function mul(uint a, uint b) returns(uint){
+      uint result =a*b;
+      return result;
+     
+  }
+   function div(uint a, uint b) returns(uint){
+      uint result =a/b;
+      return result;
+  }
+}
